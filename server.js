24dfdf5ddd7cbcb4a236610c804ee42a19917487
shahdawar <shahzebahmadislamian@gmail.com>
2026-06-17 16:10:09 +0500

    server update

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get("/", (req, res) => {
 });
 
 // ============ START SERVER ============
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // First connect to database, then start the server
 const startServer = async () => {
