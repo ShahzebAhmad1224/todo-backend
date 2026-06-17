56e50c813295e563e8bc51690dc03aaeda283506
shahdawar <shahzebahmadislamian@gmail.com>
2026-06-17 17:28:30 +0500

    cores update

--- server.js
+++ server.js
@@ -34,8 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 // 3. Enable CORS (allows frontend to make requests to backend)
 app.use(
   cors({
-    origin:
-      process.env.FRONTEND_URL || "https://todo-frontend-zeb2.vercel.app/",
+    origin: ["http://localhost:5173", "https://todo-frontend-zeb2.vercel.app"],
     credentials: true,
   }),
 );
